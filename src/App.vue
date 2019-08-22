d520f5646540c0c32f2839223506cd38cffe0e84
Tom Rajkovich <tnrajkovich@gmail.com>
2019-08-21 20:19:03 -0500

    Modified some theme elements

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,6 @@
   <div id="app">
     <header class="header header-transparent">
       <!-- Top bar-->
-      <div class="top-bar">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-6"></div>
-            <div class="col-md-6">
-              <ul class="top-bar-tools">
-                <li>
-                  <a class="open-offcanvas" href="#"><i class="ti-menu"></i></a>
-                </li>
-                <li>
-                  <a href="sign-in.html"><i class="ti-search"></i></a>
-                </li>
-                <li>
-                  <a href="sign-in.html"><i class="ti-user"></i></a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
 
       <div class="navigation navigation-center">
         <div class="container-fluid">
@@ -159,85 +139,18 @@
           </div>
           <div class="col-md-3">
             <!-- Recent entries widget-->
-            <aside class="widget widget_recent_entries">
-              <div class="widget-title">
-                <h5>Recent Posts</h5>
+            <div class="small-footer">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="copyright">
+                      © 2019
+                      <a href="http://2the.me/">2theme</a>
+                      All Rights Reserved.
+                    </div>
+                  </div>
+                </div>
               </div>
-              <ul>
-                <li>
-                  <a href="#">Matcha Portuguese Egg Tarts</a>
-                  <span class="post-date">May 8, 2016</span>
-                </li>
-                <li>
-                  <a href="#">Camp Cooking: One Pan Yaki Udon</a>
-                  <span class="post-date">April 7, 2016</span>
-                </li>
-                <li>
-                  <a href="#">Chicken & Spring Vegetable Lasagna</a>
-                  <span class="post-date">April 7, 2016</span>
-                </li>
-                <li>
-                  <a href="#">Mini California Sushi Cones</a>
-                  <span class="post-date">April 7, 2016</span>
-                </li>
-              </ul>
-            </aside>
-          </div>
-          <div class="col-md-3">
-            <!-- Recent portfolio widget-->
-            <aside class="widget widget_recent_works">
-              <div class="widget-title">
-                <h5>Instagram</h5>
-              </div>
-              <ul>
-                <li>
-                  <a href="#"><img src="assets/images/widgets/1.jpg" alt="" /></a>
-                </li>
-                <li>
-                  <a href="#"><img src="assets/images/widgets/2.jpg" alt="" /></a>
-                </li>
-                <li>
-                  <a href="#"><img src="assets/images/widgets/3.jpg" alt="" /></a>
-                </li>
-                <li>
-                  <a href="#"><img src="assets/images/widgets/4.jpg" alt="" /></a>
-                </li>
-                <li>
-                  <a href="#"><img src="assets/images/widgets/5.jpg" alt="" /></a>
-                </li>
-                <li>
-                  <a href="#"><img src="assets/images/widgets/6.jpg" alt="" /></a>
-                </li>
-              </ul>
-            </aside>
-          </div>
-        </div>
-      </div>
-      <div class="small-footer">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="copyright">
-                © 2019
-                <a href="http://2the.me/">2theme</a>
-                All Rights Reserved.
-              </div>
-            </div>
-            <div class="col-md-6">
-              <ul class="social-links">
-                <li>
-                  <a href="#"><i class="fa fa-facebook"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-vimeo"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-twitter"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-pinterest"></i></a>
-                </li>
-              </ul>
             </div>
           </div>
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,24 +19,6 @@
 img {
   border-radius: 20px 20px 0 0;
 }
-
-#myVideo {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  min-width: 100%;
-  min-height: 100%;
-}
-
-/* Add some content at the bottom of the video/page */
-.content {
-  position: fixed;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  color: #f1f1f1;
-  width: 100%;
-  padding: 20px;
-}
 </style>
 
 <script>
