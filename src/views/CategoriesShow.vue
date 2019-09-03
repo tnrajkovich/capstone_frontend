03c2967b465a32102382cbdcfc220449f27c13c5
Tom Rajkovich <tnrajkovich@gmail.com>
2019-09-03 15:54:14 -0500

    Up to date

---
+++ src/views/CategoriesShow.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 m-auto text-center" v-for="parent_recipe in category.parent_recipes">
+        <img :src="parent_recipe.image_url" />
+        <h1 class="display-1">
+          <router-link v-bind:to="`/parent_recipes/${parent_recipe.id}`">{{ parent_recipe.title }}</router-link>
+        </h1>
+        <p class="lead">{{ parent_recipe.ingredients }}</p>
+        <p class="lead">{{ parent_recipe.directions }}</p>
+        <div class="divider-border"></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="space" data-mY="60px"></div>
+      </div>
+    </div>
+
+    <router-link v-bind:to="`/`">Back to all recipes</router-link>
+  </div>
+</template>
+
+<style type="text/css">
+img {
+  width: 300px;
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      category: {},
+      recipe: {},
+      parent_recipe: {},
+      user_recipe: {},
+      user: {},
+      description: "",
+      new_ingredients: "",
+      parent_recipe_id: "",
+      vote: "",
+      image_url: "",
+      errors: [],
+      username: null
+    };
+  },
+  created: function() {
+    axios.get("/api/categories/" + this.$route.params.id).then(response => {
+      this.category = response.data;
+    });
+    this.username = localStorage.getItem("username");
+  },
+  methods: {}
+};
+</script>
